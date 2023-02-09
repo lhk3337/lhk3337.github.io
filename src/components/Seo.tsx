431598bbfca1b6cd7023b30cec6a6751b26dddde
lhk3337 <lhk3337@naver.com>
2023-02-09 10:52:08 +0900

    [FEAT] google & naver SEO 설정하기 & Seo로 이동

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -4,11 +4,41 @@ interface Props {
   title: string;
 }
 export default function Seo({ title: pageTitle }: Props) {
-  const { title: siteTitle } = useSiteMetadata();
+  const {
+    title: siteTitle,
+    siteUrl,
+    description,
+    file: { publicURL },
+  } = useSiteMetadata();
 
   return (
-    <title>
-      {pageTitle} | {siteTitle}
-    </title>
+    <>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:image" content={publicURL} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content={siteTitle} />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@author_handle" />
+      <meta name="twitter:title" content={siteUrl} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:creator" content="@author_handle" />
+      <meta name="twitter:image" content={publicURL} />
+
+      <meta name="google-site-verification" content="-bEp6qApLyHi109x85kjQCHSLAp15SRSlaTTqvRKm70" />
+
+      <meta name="naver-site-verification" content="c78512d8a5b78f43ecd4ea7fa1ab32e00c938ab7" />
+
+      <html lang="ko" />
+      <title>
+        {pageTitle} | {siteTitle}
+      </title>
+    </>
   );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,34 +7,8 @@ interface LayoutProps {
   location?: string;
 }
 export default function Layout({ children, location }: LayoutProps) {
-  const {
-    title: siteTitle,
-    siteUrl,
-    description,
-    file: { publicURL },
-  } = useSiteMetadata();
   return (
     <main>
-      <Helmet>
-        <meta name="description" content={description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={siteTitle} />
-        <meta property="og:url" content={siteUrl} />
-        <meta property="og:image" content={publicURL} />
-        <meta property="og:description" content={description} />
-        <meta property="og:site_name" content={siteTitle} />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@author_handle" />
-        <meta name="twitter:title" content={siteUrl} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:creator" content="@author_handle" />
-        <meta name="twitter:image" content={publicURL} />
-        <html lang="ko" />
-      </Helmet>
       <Nav location={location} />
       {children}
     </main>
