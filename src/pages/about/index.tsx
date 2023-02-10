566bf07c86078ac7bdef5c6122957e1bd90d1b4c
lhk3337 <lhk3337@naver.com>
2023-02-10 19:24:12 +0900

    [FIX] about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -6,7 +6,7 @@ export default function About({ location }: PageProps) {
   return (
     <Layout location={location.pathname}>
       <div className="mx-auto mt-16 max-w-6xl px-6 sm:px-14">
-        <span>About me</span>
+        <h1 className="text-2xl font-black sm:text-4xl ">About</h1>
       </div>
     </Layout>
   );
