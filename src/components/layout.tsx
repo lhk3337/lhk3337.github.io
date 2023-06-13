e9b8eab3167aac29790a13f934172f82a3ef43c7
lhk3337 <lhk3337@naver.com>
2023-06-13 16:59:29 +0900

    [FIX] top scroll시 scroll up button 사라지게 설정하기

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,30 @@
 import React, { ReactNode, useEffect, useState } from "react";
 import Nav from "components/nav";
+import { cls } from "libs/cls";
 interface LayoutProps {
   children: ReactNode;
   location?: string;
 }
 export default function Layout({ children, location }: LayoutProps) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+      if (scrollPosition > 0 && !isVisible) {
+        setIsVisible(true);
+      } else if (scrollPosition === 0 && isVisible) {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isVisible]);
+
   useEffect(() => {
     window.scroll({
       top: 0,
@@ -23,14 +43,12 @@ export default function Layout({ children, location }: LayoutProps) {
       {children}
       <button
         onClick={uptoScroll}
-        className="fixed bottom-4 right-4 inline-flex h-10 w-10 items-center justify-center rounded-full bg-[#F3F3F3] shadow-md dark:text-black md:right-8 md:bottom-8 md:h-14 md:w-14 md:text-2xl"
+        className={cls(
+          isVisible ? "opacity-100" : "opacity-0",
+          "fixed bottom-4 right-4 inline-flex h-10 w-10 items-center justify-center rounded-full bg-[#F3F3F3] shadow-md transition-opacity duration-300 ease-in-out dark:text-black md:bottom-12 md:right-12 md:h-12 md:w-12 md:text-2xl"
+        )}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="h-10 w-10 md:h-8 md:w-8 "
-        >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
           <path
             fillRule="evenodd"
             d="M11.47 2.47a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06l-6.22-6.22V21a.75.75 0 01-1.5 0V4.81l-6.22 6.22a.75.75 0 11-1.06-1.06l7.5-7.5z"
@@ -41,3 +59,5 @@ export default function Layout({ children, location }: LayoutProps) {
     </main>
   );
 }
+
+// isVisible ? "block" : "hidden";
